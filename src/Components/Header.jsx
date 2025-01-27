16f4caef8dc3edd1a5e99375a18a6296a491afb0
pravin padalkar <pravinpadalkar78@gmail.com>
2025-01-27 21:41:10 +0530

    Modification

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center bg-gray-50 shadow-md px-4 md:px-8 py-2 md:py-4">
+    <div className="flex justify-between items-center bg-gray-50 shadow-md px-1 md:px-8 py-1 md:py-4">
       <div className="flex gap-1 md:gap-4">
         <Link to="/">
           <img className="min-w-10" src={viteLogo} alt="logo" />
         </Link>
-        <Link to="" className="text-2xl md:text-3xl text-indigo-400">
-          <h1>Actify</h1>
+        <Link to="" className="text-lg md:text-3xl text-indigo-400">
+          <h1>Custom Dashboard</h1>
         </Link>
       </div>
       <div className="flex gap-2 text-sm md:text-base">
@@ -18,7 +18,7 @@ export default function Header() {
           to=""
           className="bg-sky-200 hover:bg-sky-300 px-1 md:px-2 py-1 rounded-sm cursor-pointer"
         >
-          AccountList
+          AccountsList
         </Link>
         <Link
           to="entryform"
